f5b19d23e43f6d5cd6ae67f47da803a5341f485c
PC-MANU\Manuu <manuu.100@hotmail.com>
2018-08-14 23:02:32 -0300

    Cambios esteticos al login y registro

--- login.php
+++ login.php
@@ -8,31 +8,36 @@
 	<title>Login - Agora</title>
 </head>
 <body>
-	<div class="container">
-		<form method="POST">
-			<h2 class="titulo">Login</h2><br>
-		  <div class="row">
-		  	<div class="col-12">
-		  		<label for="inputEmail1">Email</label>
-		    	<input type="email" class="form-control" id="inputEmail1"placeholder="Email" name="email">
-		  	</div> 
-		  </div>
-		  <div class="row">
-		  	<div class="col-12">
-		  		<label for="inputPassword">Contraseña</label>
-		    	<input type="password" class="form-control" id="inputPassword" aria-describedby="passwordHelp" placeholder="Contraseña" name="password">
-		    	<a href="#"><small id="passwordHelp" class="form-text">¿Olvidaste tu contraseña?</small></a>
-		  	</div>
-		  </div>
-		  <div class="row form-check">
-		  	<div class="col-12">
-		  		<input type="checkbox" class="form-check-input" id="checkMantenerSesion" name="mantener">
-		    	<label class="form-check-label" for="checkMantenerSesion">Mantener iniciada la sesión</label>
-		  	</div><br>
-		  </div>
-		  <button type="submit" class="btn btn-outline-primary">Iniciar sesión</button><br>
-		  <a href="registro.php">¿Todavía no tenes una cuenta? Registrate gratis acá</a>
-		</form>
+	<div class="container-fluid">
+		<div class="linearLayoutV loginForm col-12 col-lg-7">
+			<form method="POST">
+				<h2 class="titulo">Login</h2><br>
+			  <div class="row">
+			  	<div class="col-12">
+			  		<label for="inputEmail1">Email</label>
+			    	<input type="email" class="form-control" id="inputEmail1"placeholder="Email" name="email">
+			  	</div> 
+			  </div>
+			  <div class="row">
+			  	<div class="col-12">
+			  		<label for="inputPassword">Contraseña</label>
+			    	<input type="password" class="form-control" id="inputPassword" aria-describedby="passwordHelp" placeholder="Contraseña" name="password">
+			    	<a href="#"><small id="passwordHelp" class="form-text">¿Olvidaste tu contraseña?</small></a>
+			  	</div>
+			  </div>
+			  <div class="row form-check">
+			  	<div class="col-12">
+			  		<input type="checkbox" class="form-check-input" id="checkMantenerSesion" name="mantener">
+			    	<label class="form-check-label" for="checkMantenerSesion">Mantener iniciada la sesión</label>
+			  	</div><br>
+			  </div>
+			  <button type="submit" class="btn btn-outline-primary">Iniciar sesión</button><br>
+			  <a href="registro.php">¿Todavía no tenes una cuenta? Registrate gratis acá</a>
+			</form>
+		</div>
+		<div class="loginImg col-lg-5">
+			<img src="img/loginMinimalistPic.png">
+		</div>
 	</div>	
 </body>
 </html>
--- registro.php
+++ registro.php
@@ -8,45 +8,51 @@
 	<title>Registro - Agora</title>
 </head>
 <body>
-	<div class="container">
-		<form method="POST">
-			<div class="row">
-				<div class="col-12">
-					<h2 class="titulo">Registro</h2><br>
+	<div class="container-fluid">
+		<div class="registerImg col-lg-5">
+			<img src="img/registerMinimalistPic.png">
+		</div>
+		<div class="linearLayoutV loginForm col-12 col-lg-7">
+			<form method="POST">
+				<div class="row">
+					<div class="col-12">
+						<h2 class="titulo">Registro</h2><br>
+					</div>
 				</div>
-			</div>
-			<div class="row">
-				<div class="col-12 col-sm-6">
-				    <label for="inputNombre">Nombre</label>
-			    	<input type="text" class="form-control" id="inputNombre"placeholder="Nombre" name="name">
-			  	</div>
-			  	<div class="col-12 col-sm-6">
-				    <label for="inputLastname">Apellido</label>
-			    	<input type="text" class="form-control" id="inputLastname"placeholder="Apellido" name="lastname">
-			  	</div>
-			</div>
-			<div class="row">
-				<div class="col-12 col-sm-6">
-				    <label for="inputEmail1">Email</label>
-			    	<input type="email" class="form-control" id="inputEmail1"placeholder="Email" name="email">
-			  	</div>
-			  	<div class="col-12 col-sm-6">
-				    <label for="inputFecha">Fecha de nacimiento</label>
-			    	<input type="text" class="form-control" id="inputFecha"placeholder="AAAA/MM/DD" name="fecha">
-			  	</div>
-			</div>
-			<div class="row">
-				<div class="col-12 col-sm-6">
-				    <label for="inputPassword">Contraseña</label>
-			    	<input type="password" class="form-control" id="inputPassword"placeholder="Elegí una contraseña" name="pass">
-			  	</div>
-			  	<div class="col-12 col-sm-6">
-				    <label for="inputPassword2">Repetí tu contraseña</label>
-			    	<input type="password" class="form-control" id="inputPassword2"placeholder="Repetí tu contraseña" name="pass2">
-			  	</div>
-			</div><br>
-		  	<button type="submit" class="btn btn-outline-primary">Registrarse</button>
-		</form>
+				<div class="row">
+					<div class="col-12 col-sm-6">
+					    <label for="inputNombre">Nombre</label>
+				    	<input type="text" class="form-control" id="inputNombre"placeholder="Nombre" name="name">
+				  	</div>
+				  	<div class="col-12 col-sm-6">
+					    <label for="inputLastname">Apellido</label>
+				    	<input type="text" class="form-control" id="inputLastname"placeholder="Apellido" name="lastname">
+				  	</div>
+				</div>
+				<div class="row">
+					<div class="col-12 col-sm-6">
+					    <label for="inputEmail1">Email</label>
+				    	<input type="email" class="form-control" id="inputEmail1"placeholder="Email" name="email">
+				  	</div>
+				  	<div class="col-12 col-sm-6">
+					    <label for="inputFecha">Fecha de nacimiento</label>
+				    	<input type="text" class="form-control" id="inputFecha"placeholder="AAAA/MM/DD" name="fecha">
+				  	</div>
+				</div>
+				<div class="row">
+					<div class="col-12 col-sm-6">
+					    <label for="inputPassword">Contraseña</label>
+				    	<input type="password" class="form-control" id="inputPassword"placeholder="Elegí una contraseña" name="pass">
+				  	</div>
+				  	<div class="col-12 col-sm-6">
+					    <label for="inputPassword2">Repetí tu contraseña</label>
+				    	<input type="password" class="form-control" id="inputPassword2"placeholder="Repetí tu contraseña" name="pass2">
+				  	</div>
+				</div><br>
+			  	<button type="submit" class="btn btn-outline-primary">Registrarse</button><br>
+			  	<a href="login.php">¿Ya tenes una cuenta?</a>
+			</form>
+		</div>
 	</div>	
 </body>
 </html>
